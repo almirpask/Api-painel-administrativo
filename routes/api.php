530b32fca12391c09caea0e7f83ab5bc7d9b9a3f
Almir Santos <almirpask@hotmail.com>
2017-05-02 19:34:33 -0300

    add passport oauth

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,11 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::group(['middleware'=>['auth:api']], function(){
+    Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+    });
+
+    Route::resource('/eventos', 'Api\EventoController');
 });
 
-Route::resource('/eventos', 'Api\EventoController');
